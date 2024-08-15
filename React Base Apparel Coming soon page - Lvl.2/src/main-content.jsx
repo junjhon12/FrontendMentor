f3c80de4a20d76d409dd60eeaee4ddf478348f8f
Quoc Bao Dinh Le <qle17@student.gsu.edu>
2024-08-15 13:40:37 -0400

    8/15/2024

---
+++ React Base Apparel Coming soon page - Lvl.2/src/main-content.jsx
@@ -0,0 +1,12 @@
+function main_content() {
+
+        return(
+            <body>
+                <main>
+                    
+                </main>
+            </body>
+        );
+}
+
+export default main_content
