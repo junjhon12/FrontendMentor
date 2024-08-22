b5ee2b37ded866be990738513d02b4b65a89b380
Quoc Bao Dinh Le <qle17@student.gsu.edu>
2024-08-22 15:09:15 -0400

    8/22/2024

--- Finished Projects/sign-up_form/src/main.jsx
+++ Finished Projects/sign-up_form/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import LoginPage from './loginPage'
+import LoginPage from './LoginPage'
 import React from 'react'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
